1b9f9f01a83cee162a1202a102ece966f2d2825b
Keitinn <keitinn.reader@gmail.com>
2017-04-14 16:10:57 +0900

    Initial commit

---
+++ bmi.py
@@ -0,0 +1,13 @@
+weight = input('体重(キロ単位)を入力してください')
+height = input('身長(メートル単位)を入力してください')
+bmi = float(weight) / (float(height) ** 2)
+
+if bmi < 18.5:
+    print ('やせている')
+
+elif bmi < 25:
+    print ('ふつう')
+elif bmi < 35:
+    print ('ちょっと太っている')
+else:
+    print ('だいぶ太っている')
---
+++ if_nested.py
@@ -0,0 +1,8 @@
+a = 20
+if a >= 10:
+    print ('aa')
+    if a >=30:
+        print('bb')
+    else:
+        print('cc')
+        
