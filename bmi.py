ca869599ea84ca29225ab28461be9c68e802ef87
Kaito Ishii <ht16a004@oecu.jp>
2017-06-30 14:55:25 +0900

    Add: BMI値の表記

--- bmi.py
+++ bmi.py
@@ -1,6 +1,6 @@
-weight = input('体重(キロ単位)を入力してください')
-height = input('身長(メートル単位)を入力してください')
-bmi = float(weight) / (float(height) ** 2)
+weight = input('体重(キロ単位)を入力してください: ')
+height = input('身長(メートル単位)を入力してください: ')
+bmi = print('あなたのBMI値は 'float(weight) / (float(height) ** 2)'です。')
 
 if bmi < 18.5:
     print ('やせている')
