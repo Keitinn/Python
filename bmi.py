3ca4b7ca95a419fc122eb7c3b22c4ffb1c7a8b65
Hidaka <ht16a068@oecu.jp>
2017-07-01 23:54:33 +0900

    追記
    bmiの値を表示させるように変更

--- bmi.py
+++ bmi.py
@@ -1,13 +1,16 @@
 weight = input('体重(キロ単位)を入力してください')
 height = input('身長(メートル単位)を入力してください')
-bmi = float(weight) / (float(height) ** 2)
+bmi = float(weight) / (float(height) **  2)
 
 if bmi < 18.5:
+    print(bmi)
     print ('やせている')
-
 elif bmi < 25:
+    print(bmi)
     print ('ふつう')
 elif bmi < 35:
+    print(bmi)
     print ('ちょっと太っている')
 else:
+    print(bmi)
     print ('だいぶ太っている')
