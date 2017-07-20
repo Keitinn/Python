b2effd5174cac4532d5d3703c801d3895cfc625a
Kaito Ishii <ht16a004@oecu.jp>
2017-07-20 17:50:26 +0900

    Change: 計算結果の表示をわかりやすく変更

--- bmi.py
+++ bmi.py
@@ -1,16 +1,22 @@
-weight = input('体重(キロ単位)を入力してください')
-height = input('身長(メートル単位)を入力してください')
-bmi = float(weight) / (float(height) **  2)
+weight = input('体重(キログラム単位)を入力してください: ')
+height = input('身長(メートル単位)を入力してください: ')
+bmi = float(weight) / (float(height) ** 2)
 
 if bmi < 18.5:
-    print(bmi)
-    print ('やせている')
+    a = round(bmi, 2)
+    style = 'やせています\n'
 elif bmi < 25:
-    print(bmi)
-    print ('ふつう')
+    a = round(bmi, 2)
+    style = 'ふつう体型です\n'
 elif bmi < 35:
-    print(bmi)
-    print ('ちょっと太っている')
+    a = round(bmi, 2)
+    style = 'すこし太っています\n'
 else:
-    print(bmi)
-    print ('だいぶ太っている')
+    a = round(bmi, 2)
+    style = 'だいぶ太っています\n'
+
+b = 'あなたは '
+
+print(b + style)
+print('あなたのBMI: ')
+print(a)
